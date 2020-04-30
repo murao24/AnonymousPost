a65b17d6ed9ad7c47af2308c20451c0d78b8900f
Yoshinobu <Yoshinobu>
2020-04-30 15:50:44 +0900

    initial commit

---
+++ AnonymousPost/Controllers/PostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostViewController.swift
+//  AnonymousPost
+//
+//  Created by 村尾慶伸 on 2020/04/30.
+//  Copyright © 2020 村尾慶伸. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
