5519b617626ef55d63edcc1564cb020946a744e9
Yoshinobu <Yoshinobu>
2020-04-30 16:30:28 +0900

    add Firebase/Auth

--- AnonymousPost/Controllers/RegisterViewController.swift
+++ AnonymousPost/Controllers/RegisterViewController.swift
@@ -7,25 +7,25 @@
 //
 
 import UIKit
+import Firebase
 
 
 class RegisterViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            
 
-        // Do any additional setup after loading the view.
+        }
+
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
